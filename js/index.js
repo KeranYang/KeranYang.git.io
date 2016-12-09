a55fc692009a8b6449b374f6d321dc624c2316fa
KeranYang <yangkr920208@gmail.com>
2016-12-08 21:49:33 -0500

    just beginning
    just beginning

--- js/index.js
+++ js/index.js
@@ -5,29 +5,23 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller("menuCtrl", function($scope) {
+myApp.controller("appCtrl", function($scope, $http) {
+	
+	$scope.showLcList = false;
+	$http.get("leetcodes.json").then(function(response) {
+        $scope.leetcodes = response.data.leetcodes;
+    });
 	
 	$scope.switchToAlgorithm = function() {
-		$("#codeDiv").load("leetcode_list.html");
+		$scope.showLcList = true;
 	}
-	
-	$scope.switchToDesignPattern = function() {
-		$("#codeDiv").load("two_sum.html");
-	}
-	
-	$scope.switchToJava = function() {
-		$("#codeDiv").load("two_sum.html");
-	}
-	
-	$scope.switchToWeb = function() {
-		$("#codeDiv").load("two_sum.html");
-	}
-	
 });
 
+/**
 myApp.controller("lcListCtrl", function($scope, $http) {
 	$http.get("leetcodes.json").then(function(response) {
         $scope.leetcodes = response.data.leetcodes;
 		alert($scope.leetcodes);
     }); 
 });
+**/
